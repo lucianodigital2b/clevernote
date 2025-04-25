e32f0a642be09f032c97faea406c7c074bf70597
luciano <luciano.damascena@digital2b.com.br>
2025-04-24 23:58:00 -0300

    r2

--- app/Models/Flashcard.php
+++ app/Models/Flashcard.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Flashcard extends Model
+class Flashcard extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $fillable = [
         'folder_id',
